e5cd5564a1ab16cc43337dc1fe5d3ac016ded21d
virajalmeida10 <almeida.viraj01@gmail.com>
2026-08-02 10:54:55 -0400

    Initial commit

---
+++ src/main/java/com/almeida/viraj01/projects/stanzaApp/StanzaAppApplication.java
@@ -0,0 +1,13 @@
+package com.almeida.viraj01.projects.stanzaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StanzaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StanzaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/almeida/viraj01/projects/stanzaApp/StanzaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almeida.viraj01.projects.stanzaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StanzaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
